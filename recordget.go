162f59d59e4374228f57c36511c7387c8b9a969c
Simon Tucker <brotherlogic@gmail.com>
2017-03-11 13:38:40 -0800

    Updated IP

--- recordget.go
+++ recordget.go
@@ -254,10 +254,11 @@ func getCard(rel *pbd.Release) pbc.Card {
 }
 
 func main() {
-	var host = flag.String("host", "10.0.1.17", "Hostname of server.")
+	var host = flag.String("host", "192.168.86.34", "Hostname of server.")
 	var port = flag.Int("port", 50055, "Port number of server")
 	var dryRun = flag.Bool("dry_run", false, "If true, takes no action")
 	var quiet = flag.Bool("quiet", true, "Don't log anything.")
+	flag.Parse()
 
 	if *quiet {
 		log.SetOutput(ioutil.Discard)
