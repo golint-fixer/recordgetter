e0b19759540a97616b7c95cf5eb19ad01c797512
Simon Tucker <brotherlogic@gmail.com>
2018-01-05 14:39:46 -0800

    Added a log line

--- recordgetterapi.go
+++ recordgetterapi.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"time"
 
 	"golang.org/x/net/context"
@@ -16,6 +17,7 @@ func (s *Server) GetRecord(ctx context.Context, in *pb.GetRecordRequest) (*pbrc.
 
 		if in.GetRefresh() {
 			rec, err := s.getRelease(ctx, s.state.CurrentPick.Release.InstanceId)
+			s.Log(fmt.Sprintf("GOT %v and %v", rec, err))
 			if err != nil && len(rec.GetRecords()) == 1 {
 				s.state.CurrentPick = rec.GetRecords()[0]
 			}
