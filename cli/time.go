feac8987a6a982a9225320e944c1851bcf9d1cf8
Simon Tucker <brotherlogic@gmail.com>
2018-02-18 17:00:49 -0800

    Fixes CLI

--- cli/time.go
+++ cli/time.go
@@ -6,14 +6,10 @@ import (
 	"log"
 	"os"
 	"strconv"
-	"time"
 
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/codes"
-	"google.golang.org/grpc/status"
 
 	pb "github.com/brotherlogic/discogssyncer/server"
-	pbdi "github.com/brotherlogic/discovery/proto"
 	pbd "github.com/brotherlogic/godiscogs"
 	"github.com/brotherlogic/goserver/utils"
 	pbrg "github.com/brotherlogic/recordgetter/proto"
@@ -23,29 +19,8 @@ import (
 )
 
 func findServer(name string) (string, int) {
-	conn, err := grpc.Dial(utils.Discover, grpc.WithInsecure())
-	if err != nil {
-		log.Fatalf("Cannot reach discover server: %v (trying to discover %v)", err, name)
-	}
-	defer conn.Close()
-
-	registry := pbdi.NewDiscoveryServiceClient(conn)
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
-	defer cancel()
-
-	re := &pbdi.RegistryEntry{Name: name}
-	r, err := registry.Discover(ctx, re)
-
-	e, ok := status.FromError(err)
-	if ok && e.Code() == codes.Unavailable {
-		log.Printf("RETRY")
-		r, err = registry.Discover(ctx, re)
-	}
-
-	if err != nil {
-		return "", -1
-	}
-	return r.Ip, int(r.Port)
+	ip, port, _ := utils.Resolve(name)
+	return ip, int(port)
 }
 
 func clear() {
