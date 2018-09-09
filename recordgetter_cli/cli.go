8842a5c98801c5546b44c545a5c3d5d9d804e8ad
Simon Tucker <brotherlogic@gmail.com>
2018-09-09 10:56:40 -0700

    Adds cli

---
+++ recordgetter_cli/cli.go
@@ -0,0 +1,101 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"time"
+
+	"github.com/brotherlogic/goserver/utils"
+	"google.golang.org/grpc"
+
+	pb "github.com/brotherlogic/discogssyncer/server"
+	pbd "github.com/brotherlogic/godiscogs"
+	pbgs "github.com/brotherlogic/goserver/proto"
+	pbrg "github.com/brotherlogic/recordgetter/proto"
+	pbt "github.com/brotherlogic/tracer/proto"
+
+	//Needed to pull in gzip encoding init
+	_ "google.golang.org/grpc/encoding/gzip"
+)
+
+func findServer(name string) (string, int) {
+	ip, port, _ := utils.Resolve(name)
+	return ip, int(port)
+}
+
+func clear() {
+	host, port := findServer("recordgetter")
+	conn, _ := grpc.Dial(host+":"+strconv.Itoa(port), grpc.WithInsecure())
+	defer conn.Close()
+	client := pbrg.NewRecordGetterClient(conn)
+	r, err := client.Force(context.Background(), &pbrg.Empty{})
+	fmt.Printf("%v and %v", r, err)
+}
+
+func listened(score int32) {
+	host, port := findServer("recordgetter")
+	conn, _ := grpc.Dial(host+":"+strconv.Itoa(port), grpc.WithInsecure())
+	defer conn.Close()
+	client := pbrg.NewRecordGetterClient(conn)
+	r, err := client.GetRecord(context.Background(), &pbrg.GetRecordRequest{})
+	if err != nil {
+		log.Fatalf("%v", err)
+	}
+	r.GetRecord().GetRelease().Rating = score
+	_, err = client.Listened(context.Background(), r.GetRecord())
+	fmt.Printf("%v", err)
+}
+
+func get(ctx context.Context) {
+	host, port := findServer("recordgetter")
+	conn, _ := grpc.Dial(host+":"+strconv.Itoa(port), grpc.WithInsecure())
+	defer conn.Close()
+	client := pbrg.NewRecordGetterClient(conn)
+
+	r, err := client.GetRecord(ctx, &pbrg.GetRecordRequest{Refresh: true})
+	fmt.Printf("%v and %v", r, err)
+}
+
+func score(ctx context.Context, value int32) {
+	host, port := findServer("recordgetter")
+	conn, _ := grpc.Dial(host+":"+strconv.Itoa(port), grpc.WithInsecure())
+	defer conn.Close()
+	client := pbrg.NewRecordGetterClient(conn)
+	r, err := client.GetRecord(ctx, &pbrg.GetRecordRequest{})
+	if err != nil {
+		log.Fatalf("Error in scoring: %v", err)
+	}
+	r.GetRecord().GetMetadata().SetRating = value
+	_, err = client.Listened(ctx, r.GetRecord())
+	fmt.Printf("%v", err)
+}
+
+func run() (int, error) {
+	host, port := findServer("discogssyncer")
+	conn, _ := grpc.Dial(host+":"+strconv.Itoa(port), grpc.WithInsecure())
+	defer conn.Close()
+	client := pb.NewDiscogsServiceClient(conn)
+	folderList := &pb.FolderList{}
+	folder := &pbd.Folder{Name: "ListeningPile"}
+	folderList.Folders = append(folderList.Folders, folder)
+	r, err := client.GetReleasesInFolder(context.Background(), folderList)
+	if err != nil {
+		return 0, err
+	}
+
+	return len(r.GetRecords()), nil
+}
+
+func main() {
+	ctx, cancel := utils.BuildContext("RecordGet-Score", "recordgetter-cli", pbgs.ContextType_MEDIUM)
+	defer cancel()
+	val, err := strconv.Atoi(os.Args[1])
+	if err != nil {
+		log.Fatalf("Error parsing num: %v", err)
+	}
+	score(ctx, int32(val))
+	utils.SendTrace(ctx, "recordgetter-cli", time.Now(), pbt.Milestone_END, "recordgetter-cli")
+}
